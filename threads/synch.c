33773676390303184d7c288c892919d89df87bb0
seoyoung059 <asdf13531@yonsei.ac.kr>
2023-05-30 15:11:23 +0900

    sync_finish

--- threads/synch.c
+++ threads/synch.c
@@ -59,18 +59,19 @@ sema_init (struct semaphore *sema, unsigned value) {
    sema_down function. */
 void
 sema_down (struct semaphore *sema) {
-	enum intr_level old_level;
+   enum intr_level old_level;
 
-	ASSERT (sema != NULL);
-	ASSERT (!intr_context ());
+   ASSERT (sema != NULL);
+   ASSERT (!intr_context ());
 
-	old_level = intr_disable ();
-	while (sema->value == 0) {
-		list_push_back (&sema->waiters, &thread_current ()->elem);
-		thread_block ();
-	}
-	sema->value--;
-	intr_set_level (old_level);
+   old_level = intr_disable ();
+   while (sema->value == 0) {
+      // list_push_back (&sema->waiters, &thread_current ()->elem);
+      list_insert_ordered(&sema->waiters, &thread_current()->elem, less_priority, NULL);
+      thread_block ();
+   }
+   sema->value--;
+   intr_set_level (old_level);
 }
 
 /* Down or "P" operation on a semaphore, but only if the
@@ -104,16 +105,19 @@ sema_try_down (struct semaphore *sema) {
    This function may be called from an interrupt handler. */
 void
 sema_up (struct semaphore *sema) {
-	enum intr_level old_level;
+   enum intr_level old_level;
 
-	ASSERT (sema != NULL);
+   ASSERT (sema != NULL);
 
-	old_level = intr_disable ();
-	if (!list_empty (&sema->waiters))
-		thread_unblock (list_entry (list_pop_front (&sema->waiters),
-					struct thread, elem));
-	sema->value++;
-	intr_set_level (old_level);
+   old_level = intr_disable ();
+   if (!list_empty (&sema->waiters)){
+      thread_unblock (list_entry (list_pop_front(&sema->waiters),
+               struct thread, elem));
+      list_sort(&sema->waiters,less_priority,NULL);
+   }
+   sema->value++;
+   thread_yield();
+   intr_set_level (old_level);
 }
 
 static void sema_test_helper (void *sema_);
