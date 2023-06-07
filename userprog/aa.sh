62c94b604d29af51dbbcb410bf332b43f92f9b03
Ubuntu <ubuntu@ip-172-31-7-27.ap-northeast-2.compute.internal>
2023-06-07 05:56:23 +0000

    syscall-exec_ing

--- userprog/aa.sh
+++ userprog/aa.sh
@@ -4,5 +4,7 @@ cd userprog
 make clean
 make
 cd build
-pintos-mkdisk filesys.dsk 10
-pintos --fs-disk filesys.dsk -p tests/userprog/args-single:args-single -- -q -f run 'args-single onearg'
+#pintos -v -k -T 60 -m 20   --fs-disk=10 -p tests/userprog/exec-once:exec-once -p tests/userprog/child-imple:child-simple -- -q   -f run exec-once
+
+pintos -v -k -T 60 -m 20   --fs-disk=10 -p tests/userprog/exec-once:exec-once -p tests/userprog/child-simple:child-simple -- -q   -f run exec-once < /dev/null 2> tests/userprog/exec-once.errors > tests/userprog/exec-once.output
+perl -I../.. ../../tests/userprog/exec-once.ck tests/userprog/exec-once tests/userprog/exec-once.result
